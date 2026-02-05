3bdcf73b7a1ca79395ec451669b804aef5912ef8
nirmalat <tamang07nirmala@gmail.com>
2026-02-05 06:46:33 +0000

    Add console app and MySQL connection

---
+++ con/Program.cs
@@ -0,0 +1,34 @@
+﻿// dotnet add package MySql.Data
+
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt = new DataTable();
+
+            string connectionString =
+                "server=host.docker.internal;port=3333;uid=root;pwd=a;database=testdb;";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var da = new MySqlDataAdapter(
+                    "select * from person;", connection))
+                {
+                    da.Fill(dt);
+                }
+            }
+
+            Console.WriteLine(
+                (string)dt.Rows[0]["name"] + " " + (int)dt.Rows[0]["age"] + "\n" +
+                (string)dt.Rows[1]["name"] + " " + (int)dt.Rows[1]["age"]
+            );
+        }
+    }
+}
